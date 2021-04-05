4233ee7501e1e2dc437edb3d5a171bd669e2da92
Matheus Henrique Rodrigues <63480597+matheushrodrigues847@users.noreply.github.com>
2021-04-05 16:07:14 -0300

    Tornando o site responsivo

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,29 @@
-const menu = document.querySelectorAll('.menu__active');
-menu[0].nextElementSibling.classList.add('ativo');
-menu[0].classList.add('ativo');
+function menuOptions(){
+    const menu = document.querySelectorAll('.menu__active');
+    menu[0].nextElementSibling.classList.add('ativo');
+    menu[0].classList.add('ativo');
 
-function initTab(event){
-    event.preventDefault();
+    function initTab(event) {
+        event.preventDefault();
 
-    event.currentTarget.classList.toggle('ativo')
-    event.currentTarget.nextElementSibling.classList.toggle('ativo')
-    
+        event.currentTarget.classList.toggle('ativo')
+        event.currentTarget.nextElementSibling.classList.toggle('ativo')
+
+    }
+    menu.forEach(item => {
+        item.addEventListener('click', initTab)
+    });
 }
-menu.forEach(item => {
-    item.addEventListener('click', initTab)
-});
+menuOptions();
+
+const botaoMenuAbrir = document.querySelector('.icone--abrirMenu');
+
+botaoMenuAbrir.addEventListener('click', ()=>{
+    document.documentElement.classList.add('menu__ativo');
+})
+
+const botaoMenuFechar = document.querySelector('.icone--fecharMenu');
+
+botaoMenuFechar.addEventListener('click',()=>{
+    document.documentElement.classList.remove('menu__ativo');
+})
